42953d27f02a24cbf3acddd3c7cd79077b09a59b
alicebugnariu1o <91591148+alicebugnariu1o@users.noreply.github.com>
2021-10-13 20:17:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 def get_longest_all_even(lst: list[int]):
     lg = len(lst)
-    secvmax, start, end = 0, 0, 0
+    secvmax, start, end = 0, 0, 0  # declaram secventa maxima, indicii de inceput si final toate initial fiind 0
 
     for i in range(0, lg):
         if lst[i] % 2 == 0:
-            st = i
+            st = i  # initializam primul si al doilea indice cu val curenta
             dr = i
             for j in range(i, lg):
-                if lst[j] % 2 == 0:
+                if lst[j] % 2 == 0:  # am gasit un element care respecta proprietatea ceruta
                     dr = j
-                    if dr - st + 1 > secvmax:
+                    if dr - st + 1 > secvmax:  # in cazul in care lungimea secventei curente este mai mare decat secv maxima de pana acum, actualizam secv
                         secvmax = dr - st + 1
                         start = st
                         end = dr
                 else:
-                    break
+                    break  # in caz contrar ne oprim deoarece acest element nu face parte din secventa
     listnrpare = lst[start: end + 1]
     return listnrpare
 
@@ -28,16 +28,12 @@ def test_get_longest_all_even():
 test_get_longest_all_even()
 
 
-def solve1():
-    n = int(input("Dati nr elemente din lista: "))
-    lst = []
-    for i in range(0, n):
-        x = int(input(f"Dati elementul de pe pozitia {i}:  "))
-        lst.append(x)
+def solve1(lst):
     print(f"cea mai lunga secventa care are toate elementele nr pare este: {get_longest_all_even(lst)}")
 
 
 def nrdivizori(n: int):
+    # calculam nr de divizori ai unui nr si il retinem in nrdiv
     nrdiv = 1
     for d in range(2, n + 1):
         if n % d == 0:
@@ -47,7 +43,7 @@ def nrdivizori(n: int):
 
 def get_longest_same_div_count(lst: list[int]):
     lg = len(lst)
-    secvmax, start, end = 0, 0, 0
+    secvmax, start, end = 0, 0, 0  # declaram secventa maxima, indicii de inceput si final toate initial fiind 0
 
     for i in range(0, lg):
         st = i
@@ -57,7 +53,7 @@ def get_longest_same_div_count(lst: list[int]):
             nrdivj = nrdivizori(lst[j])
             if nrdivi == nrdivj:
                 dr = j
-                if dr - st + 1 > secvmax:
+                if dr - st + 1 > secvmax:  # in cazul in care lungimea secventei curente este mai mare decat secv maxima de pana acum, actualizam secv
                     secvmax = dr - st + 1
                     start = st
                     end = dr
@@ -76,19 +72,15 @@ def test_get_longest_same_div_count():
 test_get_longest_same_div_count()
 
 
-def solve2():
-    n = int(input("Dati nr elemente din lista: "))
-    lst = []
-    for i in range(0, n):
-        x = int(input(f"Dati elementul de pe pozitia {i}:  "))
-        lst.append(x)
+def solve2(lst):
     print(
         f"cea mai lunga secventa in care toate elementele au acelasi nr de divizori este: {get_longest_same_div_count(lst)}")
 
 
 def get_longest_product_is_odd(lst: list[int]):
     lg = len(lst)
-    secvmax, start, end = 0, 0, 0
+    secvmax, start, end = 0, 0, 0  # declaram secventa maxima, indicii de inceput si final toate initial fiind 0
+
     for i in range(0, lg):
         st = i
         dr = i
@@ -97,7 +89,7 @@ def get_longest_product_is_odd(lst: list[int]):
             produs = produs * lst[j]
             if produs % 2 == 1:
                 dr = j
-                if dr - st + 1 > secvmax:
+                if dr - st + 1 > secvmax:  # in cazul in care lungimea secventei curente este mai mare decat secv maxima de pana acum, actualizam secv
                     secvmax = dr - st + 1
                     start = st
                     end = dr
@@ -112,34 +104,36 @@ def test_get_longest_product_is_odd():
     assert get_longest_product_is_odd([11, 22, 16]) == [11]
 
 
-def solve3():
-    n = int(input("Dati nr elemente din lista: "))
-    lst = []
-    for i in range(0, n):
-        x = int(input(f"Dati elementul de pe pozitia {i}:  "))
-        lst.append(x)
+def solve3(lst: list[int]):
     print(
         f"cea mai lunga secventa in care toate elementele au acelasi nr de divizori este: {get_longest_product_is_odd(lst)}")
 
 
 def main():
     shouldRun = True
+    lst = []
     while shouldRun:
+
         nr = input("Alegeti problema pe care vreti sa o rezolvati: ")
+        n = int(input("Dati nr elemente din lista: "))  # citire
+        for i in range(0, n):
+            x = int(input(f"Dati elementul de pe pozitia {i}:  "))
+        lst.append(x)
         if nr == "1":
             print("Rezolvati problema 1: Cea mai lunga secventa de numere pare dintr o lista: \n")
-            solve1()
+            solve1(lst)
         elif nr == "2":
             print("Rezolvati problema 2: Cea mai lunga secventa de elemente care au acelasi nr de divizori: ")
-            solve2()
+            solve2(lst)
         elif nr == "3":
             print("Rezolvi problema 3: cea mai lunga secventa care are produsul elementelor nr.impar: ")
-            solve3()
+            solve3(lst)
         elif nr == "x":
             shouldRun = False
         else:
             print("Ati ales optiunea gresita! Reincercati! ")
 
 
-if __name__ == "_main_" :
+if __name__ == "__main__":
     main()
+
